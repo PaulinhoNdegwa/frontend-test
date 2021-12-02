6aa8889cf7c43869eff873cf987bb21948897470
Paul Gichuki <pherndegz@gmail.com>
2021-12-02 16:21:40 +0300

    feat: setup service to fetch data from API and set it to state

---
+++ types/index.ts
@@ -0,0 +1,13 @@
+export type Hustle = {
+    name: String,
+    shop_logo: String,
+    shop_banner: String
+}
+export type Product = {
+    name: String,
+    product_image: String,
+    price: String
+}
+export type Category = String
+
+export type SoryBy = String
